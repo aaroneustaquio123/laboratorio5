03af632ae1915909bb9e63196727921db4ef770e
Abel Aaron <142622615+aaroneustaquio123@users.noreply.github.com>
2023-09-19 20:40:35 -0500

    Add files via upload

---
+++ src/laboratorio/pkg05/Estudiante.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package laboratorio.pkg05;
+
+/**
+ *
+ * @author diego
+ */
+public class Estudiante {
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the apellido
+     */
+    public String getApellido() {
+        return apellido;
+    }
+
+    /**
+     * @param apellido the apellido to set
+     */
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    /**
+     * @return the codigo
+     */
+    public String getCodigo() {
+        return codigo;
+    }
+
+    /**
+     * @param codigo the codigo to set
+     */
+    public void setCodigo(String codigo) {
+        this.codigo = codigo;
+    }
+    String getTexto(){
+        return this.apellido + ", "+this.nombre+ " | Cod:  "+this.codigo;
+    }
+    
+    private String nombre;
+    private String apellido;
+    private String codigo;
+    public Estudiante(){
+        
+    }
+}
---
+++ src/laboratorio/pkg05/Laboratorio05.java
@@ -0,0 +1,19 @@
+
+package laboratorio.pkg05;
+
+/**
+ *
+ * @author diego
+ */
+public class Laboratorio05 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         FrameEstudiantes estudiante = new FrameEstudiantes();
+         estudiante.setLocationRelativeTo(null);
+         estudiante.setVisible(true);
+    }
+    
+}
